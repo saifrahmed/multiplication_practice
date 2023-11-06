cee6bcb161f394221f969d3bd32e3a9f12e25004
Saif Ahmed <saif.r.ahmed@gmail.com>
2023-11-05 20:04:45 -0500

    includes scoring

--- baby_proper.py
+++ baby_proper.py
@@ -11,33 +11,37 @@ question_list=[
 
 
 
-
+print("\n\n\n\n")
 name = input ("What is your name? ")
-print(f"Hello {name}, welcome to the facts game! Answer a question correctly and you get 3 points. Answer incorrectly and you lose 1 point. You have 30 seconds to answer as many questions as you can. Good luck! Write with caps and the beginning of the word and the rest in lowercase.")
+print(f"Hello {name}, welcome to the facts game! Answer a question correctly and you get 3 points. Answer incorrectly and you lose 1 point. You have 30 seconds to answer as many questions as you can. Good luck!")
 
 def game():
+    points = 0
     random.shuffle(question_list)
     for (random_a, random_q) in question_list:
+        print("\n")
         print(random_q)
         userinput = input("Your answer: ")
-        if userinput == random_a:
+        if userinput.upper() == random_a.upper():
             print("Correct! You have 3 points!")
+            points += 3
         else: 
             print(f"Incorrect! You lose 1 point! The correct answer is {random_a}")
+            points -= 1
 
-
+    print(f"Game over! You have {points} points!")
     sys.exit()
 
     
 while True:
     userinput = input ("Type START to start the game. ")
-    if userinput == "START":
+    if userinput.upper() == "START":
         game()
 
     else:
         print("Wait what?! Type START!")
 
-    if userinput == "END":
-        print("buh bye")
+    if userinput.upper() == "END":
+        print("See you next time!")
         sys.exit()
 
